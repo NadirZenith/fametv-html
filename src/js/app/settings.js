2b6591518de09ed55b7a436e127133604ad530cf
NadirZenith <2cb.md2@gmail.com>
2016-02-15 19:21:30 +0100

    clean

--- src/js/app/settings.js
+++ src/js/app/settings.js
@@ -10,11 +10,18 @@ if (debug) {
         templatePath: site_path + '/fametv-html/src/js/app/tpls/',
     };
 } else {
-
+/*
+ * 
     var config = {
         rootPath: '/fame/fametv/web/app.php',
         apiEntryPoint: site_path + '/fame/fametv/web/app.php/api/',
         templatePath: site_path + '/fametv-html/src/js/app/tpls/',
+    };
+ */
+    var config = {
+        rootPath: '/',
+        apiEntryPoint: site_path + '/',
+        templatePath: site_path + '/bundles/app/front-end/src/js/app/tpls/',
     };
 
 }
